271549b043ca90c88c0da1fc817a73cde3530ad7
Brian Ondov <ondovb@green.local>
2011-03-08 16:18:34 -0500

    Added tar script

---
+++ tar.sh
@@ -0,0 +1,16 @@
+tar -cf KronaTools.tar \
+	KronaTools/scripts/extractTaxonomy.pl \
+	KronaTools/scripts/getContigMagnitudesCA.pl \
+	KronaTools/scripts/getContigMagnitudesNewbler.pl \
+	KronaTools/scripts/importBLAST.pl \
+	KronaTools/scripts/importMETAREP.pl \
+	KronaTools/scripts/importMGRAST.pl \
+	KronaTools/scripts/importPhymmBL.pl \
+	KronaTools/scripts/importText.pl \
+	KronaTools/scripts/importXML.pl \
+	KronaTools/scripts/indexGIs.pl \
+	KronaTools/lib/Krona.pm \
+	KronaTools/src/krona.js \
+	KronaTools/img/hidden.png \
+	KronaTools/install.pl \
+	KronaTools/updateTaxonomy.sh
