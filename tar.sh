b5265eec130ff92ba8bf64f43b558fce36185916
Brian Ondov <ondovb@nbacc.net>
2015-08-18 18:26:58 -0400

    small logo in tar.sh

--- tar.sh
+++ tar.sh
@@ -39,8 +39,8 @@ tar -cnf $release.tar \
 	$release/img/favicon.uri \
 	$release/img/loading.gif \
 	$release/img/loading.uri \
-	$release/img/logo.png \
-	$release/img/logo.uri \
+	$release/img/logo-small.png \
+	$release/img/logo-small.uri \
 	$release/taxonomy \
 	$release/install.pl \
 	$release/updateTaxonomy.sh \
