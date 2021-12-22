d1479b305b1052ce54d43d395a34283319928d48
Brian Ondov <brian.ondov@gmail.com>
2021-12-22 12:30:54 -0500

    show magnitude option

--- KronaTools/src/krona-2.0.js
+++ KronaTools/src/krona-2.0.js
@@ -81,6 +81,7 @@ var svg; // for snapshot mode
 var collapse = true;
 var collapseCheckBox;
 var collapseLast;
+var showMagnitude = false;
 var compress;
 var compressCheckBox;
 var maxAbsoluteDepthText;
@@ -108,6 +109,7 @@ var searchResults;
 var nSearchResults;
 var useHueCheckBox;
 var useHueDiv;
+var showMagnitudeCheckBox;
 var datasetDropDown;
 var datasetButtonLast;
 var datasetButtonPrev;
@@ -1715,6 +1717,11 @@ function Node()
 		{
 			label = this.name;
 		}
+		
+		if (showMagnitude)
+		{
+    		label += ' [' + this.magnitude + ']'
+    	}
 		
 		var flipped = drawTextPolar
 		(
@@ -3675,6 +3682,13 @@ and including collapsed wedges.'
 	position = addOptionElement
 	(
 		position,
+		'<input type="checkbox" id="showMagnitude" checked="checked" />Show magnitudes',
+		'Show magnitudes in labels'
+	);
+	
+	position = addOptionElement
+	(
+		position,
 		'<input type="checkbox" id="collapse" checked="checked" />Collapse',
 		'Collapse wedges that are redundant (entirely composed of another wedge)'
 	);
@@ -4618,6 +4632,7 @@ function showLink()
 		getGetString('dataset', currentDataset, false),
 		getGetString('node', selectedNode.id, false),
 		getGetString('collapse', collapse, true),
+		getGetString('showMagnitude', showMagnitude, true),
 		getGetString('color', useHue(), true),
 		getGetString('depth', maxAbsoluteDepth - 1, false),
 		getGetString('font', fontSize, false),
@@ -5063,6 +5078,10 @@ function load()
 			{
 				case 'collapse':
 					collapse = pair[1] == 'true';
+					break;
+				
+				case 'showMagnitude':
+					showMagnitude = pair[1] == 'true';
 					break;
 				
 				case 'color':
@@ -5695,6 +5714,9 @@ function setCallBacks()
 	canvas.onmousedown = mouseClick;
 	document.onmouseup = mouseUp;
 	keyControl.onclick = toggleKeys;
+	showMagnitudeCheckBox = document.getElementById('showMagnitude');
+	showMagnitudeCheckBox.checked = showMagnitude;
+	showMagnitudeCheckBox.onclick = handleResize;
 	collapseCheckBox = document.getElementById('collapse');
 	collapseCheckBox.checked = collapse;
 	collapseCheckBox.onclick = handleResize;
@@ -6214,6 +6236,7 @@ function update()
 		mouseY = -1;
 		
 		collapse = collapseCheckBox.checked;
+		showMagnitude = showMagnitudeCheckBox.checked;
 		compress = true;//compressCheckBox.checked;
 		shorten = true;//shortenCheckBox.checked;
 		
@@ -6593,6 +6616,11 @@ function updateNavigationButtons()
 function useHue()
 {
 	return useHueCheckBox && useHueCheckBox.checked;
+}
+
+function showMagnitudes()
+{
+	return showMagnitudesCheckBox && showMagnitudesCheckBox.checked;
 }
 /*
 function zoomOut()
